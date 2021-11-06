1c8bee597b5c16360342c73519aab362fb6152d6
Audun Kvasbø <audun@kvasbo.no>
2021-11-06 13:24:15 +0100

    Wrap nowcast

--- src/class.tellulf.php
+++ src/class.tellulf.php
@@ -1,7 +1,7 @@
 <?php
 /**
-*	The main command structure
-*/
+ *    The main command structure
+ */
 
 namespace kvasbo\tellulf;
 
@@ -11,54 +11,60 @@ require_once "./class.s3.php";
 
 class Tellulf
 {
-	private $weather;
-	private $events;
-	private $birthdays;
-	private $s3;
-	
-	public function __construct() {
-		$this->weather = new Weather();
-		$this->events = Calendar::Fetch($_ENV["CAL_FELLES"]);
-		$this->birthdays = Calendar::Fetch($_ENV["CAL_BIRTHDAYS"]);
-		$this->s3 = new s3();
-	}
-	
-	/** 
-	*	Generate N days ahead
-	*/
-	public function Generate_Coming_Days(int $number_of_days = 4) {
-		$days = [];
-		for ($i = 1; $i <= $number_of_days; $i++) {
-			// Build return array
-			$days[] = $this->Get_Data_For_Date(new \DateTime("today + $i days"));
-		}
-		$this->s3->Store_Array("today.json", $days);
-		return $days;
-	}
-	
-	public function Generate_Today() {
-		return $this->Get_Data_For_Date(new \DateTime("today"));
-	}
-	
-	private function Get_Data_For_Date(\Datetime $datetime) {
-		$date = $datetime->format('Y-m-d');
-		return array(
-			'date' => static::Create_Nice_Date($datetime),
-			'forecast' => !empty($this->weather->forecast[$date]) ? $this->weather->forecast[$date] : [],
-			'events' => !empty($this->events[$date]) ? $this->events[$date] : [],
-			'birthdays' => !empty($this->birthdays[$date]) ? $this->birthdays[$date] : []
-		);
-	}
-	
-	public function Get_Nowcast() {
-		return $this->weather->nowcast;
-	}
-	
-	private static function Create_Nice_Date(\Datetime $datetime) {
-		
-		$dager = ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag' ];
-		$weekday = $datetime->format("w");
-		return $dager[$weekday]." ".$datetime->format('j.');
-	}
-	
+    private $weather;
+    private $events;
+    private $birthdays;
+    private $s3;
+
+    public function __construct()
+    {
+        $this->weather = new Weather();
+        $this->events = Calendar::Fetch($_ENV["CAL_FELLES"]);
+        $this->birthdays = Calendar::Fetch($_ENV["CAL_BIRTHDAYS"]);
+        $this->s3 = new s3();
+    }
+
+    /**
+     *    Generate N days ahead
+     */
+    public function Generate_Coming_Days(int $number_of_days = 4)
+    {
+        $days = [];
+        for ($i = 1; $i <= $number_of_days; $i++) {
+            // Build return array
+            $days[] = $this->Get_Data_For_Date(new \DateTime("today + $i days"));
+        }
+        $this->s3->Store_Array("today.json", $days);
+        return $days;
+    }
+
+    public function Generate_Today()
+    {
+        return $this->Get_Data_For_Date(new \DateTime("today"));
+    }
+
+    private function Get_Data_For_Date(\Datetime $datetime)
+    {
+        $date = $datetime->format('Y-m-d');
+        return array(
+            'date' => static::Create_Nice_Date($datetime),
+            'forecast' => !empty($this->weather->forecast[$date]) ? $this->weather->forecast[$date] : [],
+            'events' => !empty($this->events[$date]) ? $this->events[$date] : [],
+            'birthdays' => !empty($this->birthdays[$date]) ? $this->birthdays[$date] : []
+        );
+    }
+
+    public function Get_Nowcast()
+    {
+        return $this->weather->Get_Current_Weather();
+    }
+
+    private static function Create_Nice_Date(\Datetime $datetime)
+    {
+
+        $dager = ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag'];
+        $weekday = $datetime->format("w");
+        return $dager[$weekday] . " " . $datetime->format('j.');
+    }
+
 }
--- src/class.weather.php
+++ src/class.weather.php
@@ -7,78 +7,102 @@ require_once "./kok/ical.php";
 class Weather
 {
 
-  public $forecast;
-  public $nowcast;
-  
-  public function __construct() {
-	  $this->forecast = static::Fetch_Forecast();
-	  $this->nowcast = static::Fetch_Nowcast();
-  }	
-	
-  private static function Fetch_Forecast()
-  {
-	// Fetch and decode JSON
-  	$data = file_get_contents($_ENV["YR_URL_FORECAST"], false, static::Get_Yr_Context());
-  	$forecast = json_decode($data);
-	  
-	$return = [];
-	  
-	foreach($forecast->properties->timeseries as $series) {
-		
-		// Parse the time and handle time zones
-		$time = strtotime($series->time);
-		$date = date( "Y-m-d", $time);
-		$date_data = \date_parse(date( "Y-m-d H:i:s", $time));
-		
-		if($date_data['hour'] % 6 === 1 && !empty($series->data->next_6_hours->details)) {
+    public $forecast;
+    public $nowcast;
 
-			// Init array
-			if (empty($return[$date])) {
-				$return[$date] = [];
-			}
-			
-			// Build return data set
-			$return[$date][$date_data['hour']] = array(
-				"symbol" => $series->data->next_6_hours->summary->symbol_code,
-				"details" => $series->data->next_6_hours->details
-			);
-			
-			// echo "<pre>".htmlentities(print_r($date_data, true))."</pre>";
-			// echo "<pre>".htmlentities(print_r($series, true))."</pre>";
-		}
-		
-	}
+    /**
+     * __construct
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->forecast = static::Fetch_Forecast();
+        $this->nowcast = static::Fetch_Nowcast();
+    }
 
-	return $return;
-	  
-  }
+    public function Get_Current_Weather(): array
+    {
 
-  private static function Fetch_Nowcast()
-  {
-  	$data = file_get_contents($_ENV["YR_URL_NOWCAST"], false, static::Get_Yr_Context());
-	$nowcast = json_decode($data);
-	return array(
-		'temperature' => $nowcast->properties->timeseries[0]->data->instant->details->air_temperature,
-		'symbol' => $nowcast->properties->timeseries[0]->data->next_1_hours->summary->symbol_code
-	);
-  }
-  
-  /**
-  *	Generate a context to make met.no happy
-  */
-  private static function Get_Yr_Context() {
-	  // Create a stream
-	  $opts = [
-		  "http" => [
-			  "method" => "GET",
-			  'user_agent' => "tellulf v6: audun@kvasbo.no"
-		  ]
-	  ];
-	  
-	  // DOCS: https://www.php.net/manual/en/function.stream-context-create.php
-	  $context = stream_context_create($opts);
-	  
-	  return $context;
-  }
-  
+        return array(
+            'temperature' => $this->nowcast[0]->data->instant->details->air_temperature,
+            'symbol' => $this->nowcast[0]->data->next_1_hours->summary->symbol_code,
+        );
+
+    }
+
+    /**
+     * Fetch_Forecast
+     *
+     * @return void
+     */
+    private static function Fetch_Forecast()
+    {
+        // Fetch and decode JSON
+        $data = file_get_contents($_ENV["YR_URL_FORECAST"], false, static::Get_Yr_Context());
+        $forecast = json_decode($data);
+
+        $return = [];
+
+        foreach ($forecast->properties->timeseries as $series) {
+
+            // Parse the time and handle time zones
+            $time = strtotime($series->time);
+            $date = date("Y-m-d", $time);
+            $date_data = \date_parse(date("Y-m-d H:i:s", $time));
+
+            if ($date_data['hour'] % 6 === 1 && !empty($series->data->next_6_hours->details)) {
+
+                // Init array
+                if (empty($return[$date])) {
+                    $return[$date] = [];
+                }
+
+                // Build return data set
+                $return[$date][$date_data['hour']] = array(
+                    "symbol" => $series->data->next_6_hours->summary->symbol_code,
+                    "details" => $series->data->next_6_hours->details,
+                );
+
+            }
+
+        }
+
+        return $return;
+    }
+
+    /**
+     * Fetch_Nowcast
+     *
+     * @return void
+     */
+    private static function Fetch_Nowcast()
+    {
+        $data = file_get_contents($_ENV["YR_URL_NOWCAST"], false, static::Get_Yr_Context());
+        $nowcast = json_decode($data);
+
+        return $nowcast->properties->timeseries;
+    }
+
+    /**
+     * Generate a context to make met.no happy
+     *
+     * @return stream_context
+     */
+    private static function Get_Yr_Context()
+    {
+        // Create a stream
+        $opts = [
+            "http" => [
+                "method" => "GET",
+                'user_agent' => "tellulf v6: audun@kvasbo.no",
+            ],
+        ];
+
+        // DOCS: https://www.php.net/manual/en/function.stream-context-create.php
+        $context = stream_context_create($opts);
+
+        return $context;
+    }
+
 }
