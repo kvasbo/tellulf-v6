49bbe6bac71b93160ac40352d9b0cc7f0125ced5
Audun Kvasbø <audun@kvasbo.no>
2021-11-10 21:00:56 +0100

    Events today, and some refactoring.

--- src/class.calendar.php
+++ src/class.calendar.php
@@ -6,6 +6,59 @@ require_once "./kok/ical.php";
 
 class Calendar
 {
+
+    private $events;
+    private $birthdays;
+
+    public function __construct()
+    {
+        $this->events = Calendar::Fetch($_ENV["CAL_FELLES"]);
+        $this->birthdays = Calendar::Fetch($_ENV["CAL_BIRTHDAYS"]);
+    }
+
+    public function Get_Events(string $date)
+    {
+
+        if (empty($this->events[$date])) {
+            return [];
+        }
+
+        $events = $this->events[$date];
+
+        $out = [];
+
+        foreach ($events as $e) {
+
+            $start = strtotime($e->dateStart);
+
+            // Full day
+            if (date("H", $start) === "00" && $e->duration() % 86400 === 0) {
+                $startDisplay = "";
+            } else {
+                $startDisplay = date("H:i", $start);
+            }
+
+            $tmp = array(
+                'time' => $startDisplay,
+                'title' => $title = $e->summary,
+            );
+            $out[] = $tmp;
+        }
+
+        return $out;
+    }
+
+    public function Get_Birthdays(string $date)
+    {
+        if (empty($this->birthdays[$date])) {
+            return [];
+        }
+
+        $events = $this->birthdays[$date];
+
+        return $events;
+    }
+
     /**
      *    Read the Ics and return an array of events.
      */
--- src/class.tellulf.php
+++ src/class.tellulf.php
@@ -12,15 +12,13 @@ require_once "./class.s3.php";
 class Tellulf
 {
     public $weather;
-    private $events;
-    private $birthdays;
+    private $calendar;
     private $s3;
 
     public function __construct()
     {
         $this->weather = new Weather();
-        $this->events = Calendar::Fetch($_ENV["CAL_FELLES"]);
-        $this->birthdays = Calendar::Fetch($_ENV["CAL_BIRTHDAYS"]);
+        $this->calendar = new Calendar();
         $this->s3 = new s3();
     }
 
@@ -43,6 +41,7 @@ class Tellulf
         $data = $this->Get_Data_For_Date(new \DateTime("today"));
         $now = date("H");
 
+        // only stuff that happens later.
         $data['detail_forecast'] = array_filter(
             $data['detail_forecast'], function ($k) {
                 $now = date("H");
@@ -54,21 +53,34 @@ class Tellulf
         return $data;
     }
 
+    /**
+     * Get_Data_For_Date
+     *
+     * @param  mixed $datetime
+     * @return void
+     */
     private function Get_Data_For_Date(\Datetime $datetime)
     {
         $date = $datetime->format('Y-m-d');
+
         $forecast = $this->weather->Get_Six_Hour_Forecasts();
         $weather_details = $this->weather->Get_Detailed_Weather();
 
         return array(
             'date' => static::Create_Nice_Date($datetime),
             'forecast' => !empty($forecast[$date]) ? $forecast[$date] : [],
-            'events' => !empty($this->events[$date]) ? $this->events[$date] : [],
-            'birthdays' => !empty($this->birthdays[$date]) ? $this->birthdays[$date] : [],
+            'events' => $this->calendar->Get_Events($date),
+            'birthdays' => $this->calendar->Get_Birthdays($date),
             'detail_forecast' => !empty($weather_details[$date]) ? $weather_details[$date] : [],
         );
     }
 
+    /**
+     * Create_Nice_Date
+     *
+     * @param  mixed $datetime
+     * @return void
+     */
     private static function Create_Nice_Date(\Datetime $datetime)
     {
 
