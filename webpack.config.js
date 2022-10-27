bc242bdd917a9cdb65bee069a237afd2aa8cf08f
Audun Kvasbø <audun@kvasbo.no>
2022-10-27 20:51:43 +0200

    Remove webpack

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-    entry: "./ts/index.ts",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-};
