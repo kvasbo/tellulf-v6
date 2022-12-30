66d7e4015feddc2afb7f87e752b66fa46c78af0a
Audun Kvasbø <audun@kvasbo.no>
2022-12-30 13:04:25 +0100

    New weather12 22 (#12)
    * Remove codesniffer
    
    * Loads of lintong
    
    * Fix forecast

--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@ namespace kvasbo\tellulf;
 define("HOMEY_FILE", "./homey.json");
 
  // Auto loader
-require __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
@@ -38,85 +38,92 @@ $twig = new \Twig\Environment($twig_loader, [
 // Main user interface
 $app->get('/', function (Request $request, Response $response, $args) {
 
-  global $twig;
-  $tellulf = new Tellulf();
+    global $twig;
+    $tellulf = new Tellulf();
 
-  $coming_days = $tellulf->Generate_Coming_Days();
-  $today = $tellulf->Generate_Today();
+    $coming_days = $tellulf->Generate_Coming_Days();
+    $today = $tellulf->Generate_Today();
 
-  $weather = $tellulf->weather->Get_Current_Weather();
-  $hourly = $tellulf->weather->Get_Hourly_Forecasts();
+    $weather = $tellulf->weather->Get_Current_Weather();
+    $hourly = $tellulf->weather->Get_Hourly_Forecasts();
 
-  $homey = Homey::Get_Latest_Data();
-  
-  $current_temp = $weather['temperature'];
+    $homey = Homey::Get_Latest_Data();
+    
+    $current_temp = $weather['temperature'];
 
-  if(!empty($homey->tempOut) && !empty($homey->age) && $homey->age < 100) {
-    $current_temp = $homey->tempOut;
-  }
+    if (!empty($homey->tempOut) && !empty($homey->age) && $homey->age < 100) {
+        $current_temp = $homey->tempOut;
+    }
 
-  $render_vars = [
-      "current_temperature" => $current_temp,
-      "current_weather_icon" => $weather['symbol'],
-      "days" => $coming_days,
-      "today" => $today,
-      "hourly_weather" => $hourly,
-  ];
+    $renderVars = [
+        "current_temperature" => $current_temp,
+        "current_weather_icon" => $weather['symbol'],
+        "days" => $coming_days,
+        "today" => $today,
+        "hourly_weather" => $hourly,
+    ];
 
-  $template_index = $twig->load("index.html");
-  $page = $template_index->render($render_vars);
-  $response->getBody()->write($page);
-  return $response;
+    $templateIndex = $twig->load("index.html");
+    $page = $templateIndex->render($renderVars);
+    $response->getBody()->write($page);
+    return $response;
   
 });
 
 // Clock
 $app->get("/time", function (Request $request, Response $response, $args) {
-  $data = array('time' => Clock::getTime(), 'date' => Clock::getDateFormatted(), 'week' => Clock::getWeek());
-  $payload = json_encode($data);
-  $response->getBody()->write($payload);
-  return $response->withHeader('Content-Type', 'application/json');
+    $data = array('time' => Clock::getTime(), 'date' => Clock::getDateFormatted(), 'week' => Clock::getWeek());
+    $payload = json_encode($data);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 // Return data from Homey
 $app->get("/homey", function (Request $request, Response $response, $args) {
-  $payload = Homey::Get_Latest_Data();
-  $response->getBody()->write(json_encode($payload));
-  return $response->withHeader('Content-Type', 'application/json');
+    $payload = Homey::Get_Latest_Data();
+    $response->getBody()->write(json_encode($payload));
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 // Receive data from Homey
-$app->get("/homey_put", function (Request $request, Response $response, $args) {
-  $params = $request->getQueryParams();
-  $params['time'] = time();
-  $toStore = json_encode($params);
-  file_put_contents(HOMEY_FILE, $toStore);
-  $response->getBody()->write($toStore);
-  return $response;
-});
+$app->get(
+    "/homey_put",
+    function (Request $request, Response $response, $args) {
+        $params = $request->getQueryParams();
+        $params['time'] = time();
+        $toStore = json_encode($params);
+        file_put_contents(HOMEY_FILE, $toStore);
+        $response->getBody()->write($toStore);
+        return $response;
+    }
+);
 
 // Return data from Entur
-$app->get("/entur", function (Request $request, Response $response, $args) {
-  $payload = Entur::Get();
-  $response->getBody()->write(json_encode($payload));
-  return $response->withHeader('Content-Type', 'application/json');
-});
+$app->get(
+    "/entur", function (Request $request, Response $response, $args) {
+        $payload = Entur::Get();
+        $response->getBody()->write(json_encode($payload));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+);
 
 // Return power prices
-$app->get("/powerprices", function (Request $request, Response $response, $args) {
-  try {
-    $year = date("Y", time());
-    $month = date("m", time());
-    $day = date("d", time());
-    $hour = date("G", time());
-    $payload = json_decode(file_get_contents("https://www.hvakosterstrommen.no/api/v1/prices/$year/$month-".$day."_NO1.json"));
-    $payload['now'] = $payload[$hour];
-  } catch (\Exception $e) {
-    $payload =[];
-  }
-  $response->getBody()->write(json_encode($payload));
-  return $response->withHeader('Content-Type', 'application/json');
-});
+$app->get(
+    "/powerprices", function (Request $request, Response $response, $args) {
+        try {
+            $year = date("Y", time());
+            $month = date("m", time());
+            $day = date("d", time());
+            $hour = date("G", time());
+            $payload = json_decode(file_get_contents("https://www.hvakosterstrommen.no/api/v1/prices/$year/$month-".$day."_NO1.json"));
+            $payload['now'] = $payload[$hour];
+        } catch (\Exception $e) {
+            $payload =[];
+        }
+        $response->getBody()->write(json_encode($payload));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+);
 
 // Run app
 $app->run();
