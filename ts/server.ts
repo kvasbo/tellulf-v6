e6701f18ccb8a064bab5c7f8d83e52cca69a428b
Audun Kvasbø <audun@kvasbo.no>
2023-03-27 19:15:03 +0200

    Some small worksies

--- ts/server.ts
+++ ts/server.ts
@@ -17,3 +17,46 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   console.log(GOOGLE_KEY);
 })
+
+accessCalendar();
+
+function accessCalendar() {
+
+  const jwtClient = new google.auth.JWT(
+    GOOGLE_KEY.client_email,
+    null,
+    GOOGLE_KEY.private_key,
+    SCOPES
+);
+
+    const calendar = google.calendar({
+        version: 'v3',
+        // project: GOOGLE_KEY.project_id,
+        auth: jwtClient
+    });
+
+   // const from = 
+
+  calendar.events.list({
+    calendarId: "kvasbo.no_ognucfh1asvpgc50mqms5tu0kk@group.calendar.google.com"
+    ,
+    timeMin: (new Date()).toISOString(),
+    // timeMax: (new Date().setDate(new Date().getDate())).toISOString(),
+    maxResults: 2000,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (error, result) => {
+    if (error) {
+      console.error(error.message)
+    } else {
+      if (result.data.items.length) {
+        result.data.items.forEach(e => {
+          console.log(e.summary);
+        });
+      } else {
+        console.log(JSON.stringify({ message: 'No upcoming events found.' }));
+      }
+    }
+  });
+
+}
