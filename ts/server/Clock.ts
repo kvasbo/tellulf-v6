4eea921cc5ff6dbbfcddfd443ae0a828aed4b37b
Audun Kvasbø <audun@kvasbo.no>
2023-03-28 21:07:15 +0200

    Update Clock.ts

--- ts/server/Clock.ts
+++ ts/server/Clock.ts
@@ -2,44 +2,23 @@ export interface TimeData {
   time: string;
   date: string;
   week: string;
+  dateRelative: string;
 }
+
+import { DateTime } from 'luxon';
 
 export class Clock {
 
-  public static getTime(time: number | null = null): TimeData {
-
+  public static getTime(
+    time: number = new Date().getTime()): TimeData 
+    {
+      const dt = DateTime.fromMillis(time).setLocale('nb');
       return {
-        time: new Date().getHours() + ':' + new Date().getMinutes(),
-        date: this.getDateFormatted(),
-        week: this.getWeek(time),
+        time: dt.toLocaleString(DateTime.TIME_SIMPLE),
+        date: dt.toFormat("cccc dd."),
+        week: dt.toFormat("W"),
+        dateRelative: dt.toRelative(), 
       }
-
-
-  }
-
-  public static getDateFormatted(): string {
-    const day = this.getDayName(new Date().getDay());
-    const date = new Date().getDate();
-    return `${day} ${date}.`;
-  }
-
-  public static getDayName(day: number): string {
-    const days = ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag'];
-    return days[day];
-  }
-
-  public static getMonthName(month: number): string {
-    const months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember'];
-    return months[month];
-  }
-
-  public static getWeek(time: number | null = null): string {
-    if (time === null) time = Date.now() / 1000;
-    const date = new Date(time * 1000);
-    date.setHours(0, 0, 0, 0);
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    const week1 = new Date(date.getFullYear(), 0, 4);
-    return String(1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7));
   }
 
 }
