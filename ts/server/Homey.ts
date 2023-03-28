1006aa308b95eac02a88daca7ce504ce8748f4cc
Audun Kvasbø <audun@kvasbo.no>
2023-03-28 13:38:43 +0200

    Homey works

---
+++ ts/server/Homey.ts
@@ -0,0 +1,63 @@
+export interface HomeyData {
+  time: number;
+  age: number;
+  [key: string]: any;
+}
+
+const fs = require('fs')
+
+// Get the Google key from the environment variable
+const HOMEY_FILE = 'homey.json';
+const HOMEY_DIR = './data';
+const HOMEY_PATH = HOMEY_DIR + '/' + HOMEY_FILE;
+
+export class Homey {
+  /**
+   * Get the latest data from Homey
+   * @returns The latest data from Homey
+   */
+  static Get_Latest_Data(): HomeyData {
+    let content: string | null = null;
+
+    try {
+      content = fs.readFileSync(HOMEY_PATH, 'utf8');
+    } catch (error) {
+      console.error('Failed to read file:', error);
+    }
+
+    let payload: HomeyData;
+
+    if (!content) {
+      payload = {
+        time: 0, age: 99999
+      }; // Set empty json
+    } else {
+      const data: HomeyData = JSON.parse(content);
+      data.age = Math.round(Date.now() / 1000 - data.time); // Set age
+      payload = data; // Just to ensure niceness
+    }
+
+    return payload;
+  }
+
+  // Store the dataset
+  static Set_Data(data: HomeyData) {
+
+    // Create directory if it doesn't exist
+    if (!fs.existsSync(HOMEY_DIR)) {
+      fs.mkdirSync(HOMEY_DIR);
+    }
+    
+    // Cast to numbers if possible.
+    for (const [key, value] of Object.entries(data)) {
+      data[key] = +value; // Map to numeric values
+    }
+
+    data.time = Math.round(Date.now() / 1000); // Set time
+
+    console.log("Setting Homey data", data);
+    const json = JSON.stringify(data);
+    fs.writeFileSync(HOMEY_PATH, json);
+  }
+
+}
--- ts/server/server.ts
+++ ts/server/server.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import path from 'path';
 import { Calendar } from './Calendar';
 import { Clock } from './Clock';
+import { Homey, HomeyData } from './Homey';
 import Twig from 'twig';
+
+require('dotenv').config();
 
 const app = express();
 
@@ -12,7 +15,8 @@ app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'twig');
 app.use(express.static('public')); // Static routes
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
-const port = 3000;
+
+const port = process.env.TELLULF_PORT ? process.env.TELLULF_PORT : 3000;
 
 app.get('/', (req, res) => {
   res.render('index.twig', {});
@@ -26,7 +30,18 @@ app.get('/calendar', (req, res) => {
   res.send(Calendar.getEvents());
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Get latest Homey data
+app.get("/homey", (req, res) => {
+  res.send(Homey.Get_Latest_Data());
+});
 
+// Store changes from Homey
+app.get("/homey_put", (req, res) => {
+  const data = req.query as HomeyData;
+  Homey.Set_Data(data);
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Tellulf listening on port ${port}`);
 })
