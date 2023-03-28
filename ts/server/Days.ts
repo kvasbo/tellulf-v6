ff7c7d92e3c3e0e9c83cc537152459d83645fd6e
Audun Kvasbø <audun@kvasbo.no>
2023-03-28 21:29:47 +0200

    Update Days.ts

--- ts/server/Days.ts
+++ ts/server/Days.ts
@@ -14,9 +14,10 @@ export class Days {
     this.calendar = new Calendar();
   }
 
-  public generateComingDays(numberOfDays: number = 12) {
+  public generateComingDays(numberOfDays: number = 10) {
     const days: any = [];
     for (let i = 0; i < numberOfDays; i++) {
+      const dt = DateTime.now().plus({ days: i });
       days.push(this.getDataForDate(new Date(new Date().setDate(new Date().getDate() + i))));
     }
     console.log(days);
@@ -28,6 +29,7 @@ export class Days {
   }
 
   private getDataForDate(jsDate: Date = new Date()): {
+    isoDate: string
     date: string;
     weekday: string;
     forecast: any;
@@ -49,6 +51,7 @@ export class Days {
     const daily = null; // this.weather.getDailyForecasts();
 
     return {
+        isoDate: date,
         date: Days.createNiceDate(jsDate),
         weekday: Days.createNiceDate(jsDate, true),
         forecast: [], //forecast[date] ? forecast[date] : {},
@@ -66,7 +69,7 @@ export class Days {
    * To be switched to using Luxon 
    */
   private static createNiceDate(jsDate: Date, relative = false): string {
-    const dt = DateTime.fromJSDate(jsDate).setLocale('nb');
+    const dt = DateTime.fromJSDate(jsDate).setLocale('nb').startOf("day");
     if (relative) {
         const today = DateTime.now().setLocale('nb').startOf("day");
         const tomorrow = today.plus({ days: 1 });
