5a9cfe89e68c990b2b8e877ac1930dd4ce3c03bf
Audun Kvasbø <audun@kvasbo.no>
2023-10-12 06:35:39 +0000

    Add suicide to MQTT client in order to restart on connection error.

--- ts/MQTT.ts
+++ ts/MQTT.ts
@@ -33,7 +33,26 @@ export class MqttClient {
             })
             .on('error', (error) => {
                 this.log('MQTT Error', error.message);
+                MqttClient.die();
+            })
+            .on('close', () => {
+                MqttClient.die();
+            })
+            .on('offline', () => {
+                MqttClient.die();
+            })
+            .on('disconnect', () => {
+                MqttClient.die();
             });
+    }
+
+    /**
+     * Die after a slightly random period, as we'll then restart!
+     */
+    private static die() {
+        setTimeout(() => {
+            process.exit(1);
+        }, Math.random() * 60000);
     }
 
     /**
