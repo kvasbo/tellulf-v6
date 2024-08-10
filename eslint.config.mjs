5506bb33bbcea15cfde77875a072bb8c9456f20d
Audun Kvasbø <audun@kvasbo.no>
2024-08-10 14:57:04 +0200

    Fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,28 +2,28 @@ import jsdoc from "eslint-plugin-jsdoc";
 import eslint from '@eslint/js';
 import globals from "globals";
 
-export default [
-  eslint.configs.recommended,
-  {
-    files: ["./src/*.js"],
-    plugins: {
-      jsdoc: jsdoc,
+export default {
+  ...eslint.configs.recommended,
+  files: ["./src/*.js"],
+  plugins: {
+    jsdoc,
+  },
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+      ...globals.es2022,
+      ...globals.es6,
+      ...globals.commonjs,
+      jquery: "$",
     },
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2022,
-        ...globals.es6,
-      },
-    },
-    rules: {
-      "no-console": 1,
-      "no-unused-vars": 1,
-      "no-undef": 1,
-      'jsdoc/require-description': 'warn'
-    },
-  }
-];
+  },
+  rules: {
+    "no-console": 'warn',
+    "no-unused-vars": 'warn',
+    "no-undef": 'warn',
+    "jsdoc/require-description": 'warn',
+  },
+};
