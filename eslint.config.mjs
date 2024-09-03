3768ba7db0a03a9b7ec9d2ad4c70f802a8713145
Audun Kvasbø <audun@kvasbo.no>
2024-09-03 14:28:15 +0200

    Remove jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
 // Import globals from the ESM module
 import globals from "globals";
-import jsdoc from "eslint-plugin-jsdoc";
 import eslint from '@eslint/js';
 
 export default [
-  jsdoc.configs['flat/recommended'],
   eslint.configs.recommended,
   {
       files: ["src/*.js", "src/*.mjs"],
@@ -21,23 +19,6 @@ export default [
           "semi": "error",
           "no-unused-vars": "error",
           "no-undef": "error",
-          // JSDoc rules
-          // TODO: Enable these rules
-          "jsdoc/require-description": 'off',
-          "jsdoc/require-param-description": 'off',
-          "jsdoc/require-returns-description": 'off',
-          "jsdoc/require-param-type": 'off',
-          "jsdoc/require-returns": 'off',
-          "jsdoc/require-returns-type": 'off',
-          "jsdoc/require-param": 'off',
-          "jsdoc/check-param-names": 'off',
-          "jsdoc/require-property-description": 'off',
-      }
-  },
-  {
-      files: ["src/Server.mjs"],
-      rules: {
-          "jsdoc/require-description": 'error',
       }
   }
 ];
